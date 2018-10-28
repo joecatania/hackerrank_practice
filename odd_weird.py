92567234261c76c3a566b78e59410367e5927b4b
joecatania <joecatania0@gmail.com>
2018-10-28 16:21:17 -0400

    dev-10282018

--- odd_weird.py
+++ odd_weird.py
@@ -2,12 +2,19 @@
 
 
 def check_num(N):
-    print("N", N)
+    #print("N", N)
 
     if N % 2 == 1:
-        print("N is odd: Weird", N)
+        #print("N is odd: Weird", N)
+        print("Weird")
     elif N >= 2 and N <= 5:
-             print("N is even: Not Weird", N)
+             print("Not Weird")
+    elif N >= 6 and N <= 20:
+             #print("N is even: Weird", N)
+             print("Weird")
+    elif N > 20:
+             #print("N is even: Not Weird", N)
+             print("Not Weird")
 
 
 N = int(input())
